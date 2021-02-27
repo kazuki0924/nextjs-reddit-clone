38cd1f50c91a810728fea651832869a758fa2ce6
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-02-27 16:12:46 +0900

    add: pagination

--- src/gen/gql.tsx
+++ src/gen/gql.tsx
@@ -17,7 +17,7 @@ export type Scalars = {
 export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
-  posts: Array<Post>;
+  posts: PaginatedPosts;
   post?: Maybe<Post>;
   me?: Maybe<User>;
 };
@@ -30,7 +30,13 @@ export type QueryPostsArgs = {
 
 
 export type QueryPostArgs = {
-  id: Scalars['Int'];
+  id: Scalars['Float'];
+};
+
+export type PaginatedPosts = {
+  __typename?: 'PaginatedPosts';
+  posts: Array<Post>;
+  hasMore: Scalars['Boolean'];
 };
 
 export type Post = {
@@ -56,7 +62,7 @@ export type User = {
 
 export type Mutation = {
   __typename?: 'Mutation';
-  createPost?: Maybe<Post>;
+  createPost: Post;
   updatePost?: Maybe<Post>;
   deletePost: Scalars['Boolean'];
   changePassword: UserResponse;
@@ -169,10 +175,10 @@ export type CreatePostMutationVariables = Exact<{
 
 export type CreatePostMutation = (
   { __typename?: 'Mutation' }
-  & { createPost?: Maybe<(
+  & { createPost: (
     { __typename?: 'Post' }
     & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title'>
-  )> }
+  ) }
 );
 
 export type ForgotPasswordMutationVariables = Exact<{
@@ -239,10 +245,14 @@ export type PostsQueryVariables = Exact<{
 
 export type PostsQuery = (
   { __typename?: 'Query' }
-  & { posts: Array<(
-    { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'textSnippet'>
-  )> }
+  & { posts: (
+    { __typename?: 'PaginatedPosts' }
+    & Pick<PaginatedPosts, 'hasMore'>
+    & { posts: Array<(
+      { __typename?: 'Post' }
+      & Pick<Post, 'id' | 'createdAt' | 'updatedAt' | 'title' | 'textSnippet'>
+    )> }
+  ) }
 );
 
 export const BaseErrorFragmentDoc = gql`
@@ -346,12 +356,15 @@ export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'q
 };
 export const PostsDocument = gql`
     query Posts($limit: Int!, $cursor: String) {
-  posts(cursor: $cursor, limit: $limit) {
-    id
-    createdAt
-    updatedAt
-    title
-    textSnippet
+  posts(limit: $limit, cursor: $cursor) {
+    hasMore
+    posts {
+      id
+      createdAt
+      updatedAt
+      title
+      textSnippet
+    }
   }
 }
     `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
-import NextLink from 'next/link';
-import React from 'react';
-import { Layout } from '../components/Layout';
-import { usePostsQuery } from '../gen/gql';
 import { createUrqlClient } from '../utils/createUrqlClient';
+import { Layout } from '../components/Layout';
+import NextLink from 'next/link';
+import React, { useState } from 'react';
+import { usePostsQuery } from '../gen/gql';
+import {
+	Flex,
+	Heading,
+	Link,
+	Stack,
+	Box,
+	Button,
+	Text,
+} from '@chakra-ui/react';
 
 const Index = () => {
-	const [{ data, fetching }] = usePostsQuery({
-		variables: { limit: 10, cursor: null },
+	const [variables, setVariables] = useState({
+		limit: 5,
+		cursor: null as null | string,
 	});
+
+	const [{ data, fetching }] = usePostsQuery({
+		variables,
+	});
+
+	if (!fetching && !data) {
+		return <div>you got query failed for some reason</div>;
+	}
+
 	return (
 		<Layout>
 			<Flex align='center'>
@@ -19,20 +37,37 @@ const Index = () => {
 				</NextLink>
 			</Flex>
 			<br />
-			{fetching && !data ? (
-				<div>...loading</div>
+			{!data && fetching ? (
+				<div>loading...</div>
 			) : (
 				<Stack spacing={8}>
-					{data.posts.map(p => (
-						<Box key={p.id} p={5} shadow='md' borderWidth='1px'>
-							<Heading fontSize='xl'>{p.title}</Heading>
-							<Text mt={4}>{p.textSnippet}</Text>
-						</Box>
-
-						// <div key={p.id}>{p.title}</div>
-					))}
+					{data!.posts.posts.map(p => {
+						return (
+							<Box key={p.id} p={5} shadow='md' borderWidth='1px'>
+								<Heading fontSize='xl'>{p.title}</Heading>
+								<Text mt={4}>{p.textSnippet}</Text>
+							</Box>
+						);
+					})}
 				</Stack>
 			)}
+			{data && data.posts.hasMore ? (
+				<Flex>
+					<Button
+						onClick={() => {
+							setVariables({
+								limit: variables.limit,
+								cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+							});
+						}}
+						isLoading={fetching}
+						m='auto'
+						my={8}
+					>
+						load more
+					</Button>
+				</Flex>
+			) : null}
 		</Layout>
 	);
 };
